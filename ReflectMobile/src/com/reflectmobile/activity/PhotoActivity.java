febc23e2cd83ae39362b4d4bbf82621144a2abdc
Zakhar Herych <zakhar.herych@gmail.com>
2014-06-20 18:07:04 -0400

    View photo and List details, stories and sounds done

--- ReflectMobile/src/com/reflectmobile/activity/PhotoActivity.java
+++ ReflectMobile/src/com/reflectmobile/activity/PhotoActivity.java
@@ -6,17 +6,16 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.reflectmobile.R;
+import com.reflectmobile.data.Memory;
 import com.reflectmobile.data.Moment;
 import com.reflectmobile.utility.NetworkManager.HttpGetImageTask;
 import com.reflectmobile.utility.NetworkManager.HttpGetTask;
@@ -27,9 +26,12 @@ public class PhotoActivity extends BaseActivity {
 
 	private String TAG = "PhotoActivity";
 	private Moment moment;
+	private Memory[] mMemories;
+	private LayoutInflater mInflater;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		hasNavigationDrawer = false;
 		setContentView(R.layout.activity_photo);
 		super.onCreate(savedInstanceState);
 
@@ -46,8 +48,10 @@ public class PhotoActivity extends BaseActivity {
 				.getLayoutParams();
 		mlp.setMargins(5, 0, 0, 0);
 
-		int momentId = getIntent().getIntExtra("moment_id", 0);
-		getIntent().getIntExtra("photo_id", 0);
+		final int momentId = getIntent().getIntExtra("moment_id", 0);
+		final int photoId = getIntent().getIntExtra("photo_id", 0);
+
+		mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 		// Retreive data from the web
 		final HttpTaskHandler getMomentHandler = new HttpTaskHandler() {
@@ -56,6 +60,38 @@ public class PhotoActivity extends BaseActivity {
 				// Parse JSON to the list of communities
 				moment = Moment.getMomentInfo(result);
 				setTitle(moment.getName());
+
+				int index = 0;
+				for (int count = 0; count < moment.getNumOfPhotos(); count++) {
+					if (moment.getPhoto(count).getId() == photoId) {
+						index = count;
+						break;
+					}
+				}
+				ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+				ImagePagerAdapter adapter = new ImagePagerAdapter(
+						PhotoActivity.this);
+				viewPager.setAdapter(adapter);
+				viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+					@Override
+					public void onPageSelected(int position) {
+						loadMemories(position);
+					}
+
+					@Override
+					public void onPageScrolled(int arg0, float arg1, int arg2) {
+					}
+
+					@Override
+					public void onPageScrollStateChanged(int arg0) {
+					}
+				});
+				viewPager.setCurrentItem(index);
+				if (index==0){
+					// Special case for the first item (page is not changed)  
+					loadMemories(0);
+				}
+				
 			}
 
 			@Override
@@ -67,18 +103,40 @@ public class PhotoActivity extends BaseActivity {
 		new HttpGetTask(getMomentHandler)
 				.execute("http://rewyndr.truefitdemo.com/api/moments/"
 						+ momentId);
+	}
 
-		// Retreive data from the web
-		final HttpTaskHandler getPhotoHandler = new HttpTaskHandler() {
+	public void loadMemories(int position) {
+		final ViewGroup memoryContainer = (ViewGroup) findViewById(R.id.memories_container);
+		memoryContainer.removeAllViews();
+		final TextView memoryCaption = (TextView) findViewById(R.id.memories_caption);
+		memoryCaption.setText("0 MEMORIES");
+
+		final HttpTaskHandler getMemoriesHandler = new HttpTaskHandler() {
 			@Override
 			public void taskSuccessful(String result) {
-				// Parse JSON to the list of communities
-				// getPhotoInfo(result);
-				ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
-				ImagePagerAdapter adapter = new ImagePagerAdapter(
-						PhotoActivity.this);
-				viewPager.setAdapter(adapter);
-				viewPager.setCurrentItem(0);
+				// Parse JSON to the list of memories
+				mMemories = Memory.getMemoriesInfo(result);
+				if (mMemories.length == 1){
+					memoryCaption.setText(mMemories.length + " MEMORY");
+				}
+				else {
+					memoryCaption.setText(mMemories.length + " MEMORIES");
+				}
+				for (int count = 0; count < mMemories.length; count++) {
+					View card = mInflater.inflate(R.layout.card_memory,
+							memoryContainer, false);
+					ImageView memoryIcon = (ImageView) card
+							.findViewById(R.id.memory_icon);
+					TextView memoryText = (TextView) card
+							.findViewById(R.id.memory_text);
+					TextView memoryInfo = (TextView) card
+							.findViewById(R.id.memory_info);
+					Memory memory = mMemories[count];
+					memoryIcon.setImageResource(memory.getResourceId());
+					memoryText.setText(memory.getContent());
+					memoryInfo.setText(memory.getInfo());
+					memoryContainer.addView(card);
+				}
 			}
 
 			@Override
@@ -87,77 +145,10 @@ public class PhotoActivity extends BaseActivity {
 			}
 		};
 
-		new HttpGetTask(getPhotoHandler)
-				.execute("http://rewyndr.truefitdemo.com/api/communities");
-	}
+		new HttpGetTask(getMemoriesHandler)
+				.execute("http://rewyndr.truefitdemo.com/api/memories?photo_id="
+						+ moment.getPhoto(position).getId());
 
-	public class OnSwipeTouchListener implements OnTouchListener {
-
-		protected final GestureDetector gestureDetector;
-
-		public OnSwipeTouchListener(Context ctx) {
-			gestureDetector = new GestureDetector(ctx, new GestureListener());
-		}
-
-		private final class GestureListener extends SimpleOnGestureListener {
-
-			private static final int SWIPE_THRESHOLD = 100;
-			private static final int SWIPE_VELOCITY_THRESHOLD = 100;
-
-			@Override
-			public boolean onDown(MotionEvent e) {
-				return true;
-			}
-
-			@Override
-			public boolean onFling(MotionEvent e1, MotionEvent e2,
-					float velocityX, float velocityY) {
-				boolean result = false;
-				try {
-					float diffY = e2.getY() - e1.getY();
-					float diffX = e2.getX() - e1.getX();
-					if (Math.abs(diffX) > Math.abs(diffY)) {
-						if (Math.abs(diffX) > SWIPE_THRESHOLD
-								&& Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-							if (diffX > 0) {
-								onSwipeRight();
-							} else {
-								onSwipeLeft();
-							}
-						}
-					} else {
-						if (Math.abs(diffY) > SWIPE_THRESHOLD
-								&& Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-							if (diffY > 0) {
-								onSwipeBottom();
-							} else {
-								onSwipeTop();
-							}
-						}
-					}
-				} catch (Exception exception) {
-					exception.printStackTrace();
-				}
-				return result;
-			}
-		}
-
-		public void onSwipeRight() {
-		}
-
-		public void onSwipeLeft() {
-		}
-
-		public void onSwipeTop() {
-		}
-
-		public void onSwipeBottom() {
-		}
-
-		@Override
-		public boolean onTouch(View arg0, MotionEvent arg1) {
-			return false;
-		}
 	}
 
 	public class ImagePagerAdapter extends PagerAdapter {
@@ -168,24 +159,6 @@ public class PhotoActivity extends BaseActivity {
 		public ImagePagerAdapter(Context context) {
 			// mContext = context;
 			mDrawables = new Drawable[moment.getNumOfPhotos()];
-			for (int count = 0; count < moment.getNumOfPhotos(); count++) {
-				final int index = count;
-
-				new HttpGetImageTask(new HttpImageTaskHandler() {
-					private int drawableIndex = index;
-
-					@Override
-					public void taskSuccessful(Drawable drawable) {
-						mDrawables[drawableIndex] = drawable;
-						notifyDataSetChanged();
-					}
-
-					@Override
-					public void taskFailed(String reason) {
-						Log.e(TAG, "Error downloading the image");
-					}
-				}).execute(moment.getPhoto(index).getImageMediumURL());
-			}
 		}
 
 		@Override
@@ -200,15 +173,51 @@ public class PhotoActivity extends BaseActivity {
 		}
 
 		@Override
-		public Object instantiateItem(ViewGroup container, int position) {
-			Context context = PhotoActivity.this;
-			ImageView imageView = new ImageView(context);
-			// int padding =
-			// context.getResources().getDimensionPixelSize(R.dimen.padding_medium);
-			imageView.setScaleType(ImageView.ScaleType.FIT_START);
-			imageView.setImageDrawable(mDrawables[position]);
-			((ViewPager) container).addView(imageView, 0);
+		public Object instantiateItem(final ViewGroup container, int position) {
+			Log.d(TAG, position + "");
+			ImageView imageView = new ImageView(PhotoActivity.this);
+			imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+			imageView.setTag(position);
+			((ViewPager) container).addView(imageView);
 
+			HttpImageTaskHandler[] httpImageTaskHandlers = new HttpImageTaskHandler[3];
+			for (int count = 0; count < 3; count++) {
+				final int index = position - 1 + count;
+
+				httpImageTaskHandlers[count] = new HttpImageTaskHandler() {
+					private int drawableIndex = index;
+
+					@Override
+					public void taskSuccessful(Drawable drawable) {
+						mDrawables[drawableIndex] = drawable;
+						ImageView imageView = (ImageView) ((ViewPager) container)
+								.findViewWithTag(drawableIndex);
+						if (imageView != null) {
+							imageView.setImageDrawable(drawable);
+						}
+					}
+
+					@Override
+					public void taskFailed(String reason) {
+						Log.e(TAG, "Error downloading the image");
+					}
+				};
+			}
+			if (mDrawables[position] == null) {
+				new HttpGetImageTask(httpImageTaskHandlers[1]).execute(moment
+						.getPhoto(position).getImageMediumURL());
+			} else {
+				imageView.setImageDrawable(mDrawables[position]);
+			}
+			if (position > 0 && mDrawables[position - 1] == null) {
+				new HttpGetImageTask(httpImageTaskHandlers[0]).execute(moment
+						.getPhoto(position - 1).getImageMediumURL());
+			}
+			if (position < moment.getNumOfPhotos() - 1
+					&& mDrawables[position + 1] == null) {
+				new HttpGetImageTask(httpImageTaskHandlers[2]).execute(moment
+						.getPhoto(position + 1).getImageMediumURL());
+			}
 			return imageView;
 		}
 
